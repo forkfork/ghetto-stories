70f7cd058284a85d8cf788509d2945254ecbe559
Timothy D <timothydowns@gmail.com>
2014-12-08 08:37:13 +1100

    Update main.go

--- main.go
+++ main.go
@@ -17,17 +17,12 @@ func handleRequest(w http.ResponseWriter, r *http.Request) {
   iteration, stream := urlParts[0], urlParts[1]
   issues := github.GetIssues(iteration, stream)
   for _, issue := range issues {
-    //complete, incomplete, unknown := parseIssueBody(issueBody)
     fmt.Fprintf(w, "%s done %d todo %d unknown %d\n", issue.Name, issue.Complete, issue.Incomplete, issue.Unknown)
   }
 }
 
 
 func main() {
-  // get request parameters
-  // request issues descriptions from github => returns list of names, blurbs
-  // parse blurbs to get progress
-
   http.HandleFunc("/", handleRequest)
   http.ListenAndServe(":8080", nil)
 }
